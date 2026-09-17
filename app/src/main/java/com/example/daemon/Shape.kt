5cd244c3d1ff0af8999ef826bcc19b4a067973ca
AmitejKS28 <amitejks@gmail.com>
2026-09-17 16:03:24 -0600

    fixing merge conflict

--- app/src/main/java/com/example/daemon/Shape.kt
+++ app/src/main/java/com/example/daemon/Shape.kt
@@ -1,3 +1,5 @@
 package com.example.daemon
 
-abstract class Shape(var x: Int, var y: Int, val color: String = "blue")
+abstract class Shape(var x: Int, var y: Int){
+    val color: String = "black"
+}
